2429b432ca588752408771bf2a2cc386ac38ee97
CristianUrsino <ursino.cristian.uc@gmail.com>
2023-10-24 15:33:35 +0200

    added second snack

--- script/script.js
+++ script/script.js
@@ -21,3 +21,48 @@ const invitati = vip.map((actualVip,i)=>{
 });
 
 console.log(invitati);
+
+//esercizio 2
+console.log('.......................................................................................');
+
+const alunni = [
+    {
+        id:213,
+        name:'Marco della Rovere',
+        grades:78
+    },
+    {
+        id:250,
+        name:'Andrea Mantegna',
+        grades:48
+    },
+    {
+        id:145,
+        name:'Gaia Borromini',
+        grades:74
+    },
+    {
+        id:196,
+        name:'Luigi Grimaldello',
+        grades:68
+    },
+    {
+        id:102,
+        name:'Piero della Francesca',
+        grades:50
+    },
+    {
+        id:120,
+        name:'Francesca da Polenta',
+        grades:84
+    }
+];
+
+const nome = alunni.map((nomeAlunno)=>nomeAlunno.name);
+console.log(nome);
+
+const over70Grades = alunni.filter((alunno)=> alunno.grades > 70);
+console.log(over70Grades);
+
+const over120IdOver70Grades = alunni.filter((alunno)=> (alunno.grades > 70) && (alunno.id > 120));
+console.log(over120IdOver70Grades);
